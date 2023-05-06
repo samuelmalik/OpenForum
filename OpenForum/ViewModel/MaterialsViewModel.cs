f7b332704a339dc90754402e59b3fecb91ea29be
samuelmalik <samuelmalik9@gmail.com>
2023-05-06 12:57:57 +0200

    navigation between Forum, Materials and Quiz works in every direction

--- OpenForum/ViewModel/MaterialsViewModel.cs
+++ OpenForum/ViewModel/MaterialsViewModel.cs
@@ -9,11 +9,18 @@ namespace OpenForum.ViewModel
 {
     public partial class MaterialsViewModel : BaseViewModel
     {
+        public MaterialsViewModel() { }
 
         [RelayCommand]
         void NavigateToForum()
         {
             Shell.Current.GoToAsync("..", false);
+        }
+
+        [RelayCommand]
+        void NavigateToQuiz()
+        {
+            Shell.Current.GoToAsync($"../{nameof(QuizPage)}", false);
         }
 
 
