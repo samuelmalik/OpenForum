f2f91339c25ead2ce64500ffcfdcf196499789b8
samuelmalik <samuelmalik9@gmail.com>
2023-05-16 05:54:15 +0200

    save

--- OpenForum/View/UsersPage.xaml.cs
+++ OpenForum/View/UsersPage.xaml.cs
@@ -27,4 +27,23 @@ public partial class UsersPage : ContentPage
         else { viewModel.IsCurrentUserAdmin = false; }
         base.OnAppearing();
     }
+
+    
+
+    protected override void OnNavigatedTo(NavigatedToEventArgs args)
+    {
+        UsersViewModel viewModel = BindingContext as UsersViewModel;
+        viewModel.Users.Clear();
+        foreach (var user in User.All)
+        {
+            viewModel.Users.Add(user);
+        }
+
+        if (User.currentUser.IsAdmin == 1)
+        {
+            viewModel.IsCurrentUserAdmin = true;
+        }
+        else { viewModel.IsCurrentUserAdmin = false; }
+        base.OnNavigatedTo(args);
+    }
 }
--- OpenForum/ViewModel/UserDetailsViewModel.cs
+++ OpenForum/ViewModel/UserDetailsViewModel.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -47,6 +48,17 @@ namespace OpenForum.ViewModel
                 int newXp = Convert.ToInt32(AddXp) + Convert.ToInt32(Xp);
                 Xp = newXp.ToString();
                 User.currentViewedUser.Xp = newXp;
+                User.currentUser.Xp = newXp;
+
+                for (int i = 0; i < User.All.Count; i++)
+                {
+                    if (User.All[i].Id == Id)
+                    {
+                        User.All[i].Xp = Convert.ToInt32(Xp);
+                        break;
+                    }
+                }
+
                 AddXp = "";
                 await UserService.UpdateXp(Id, Convert.ToInt32(Xp));
                 }
@@ -58,8 +70,26 @@ namespace OpenForum.ViewModel
         {
             if (SetXp != "") {
             Xp = Convert.ToInt32(SetXp).ToString();
-                User.currentViewedUser.Xp = Convert.ToInt32(Xp);
-            SetXp = "";
+            User.currentViewedUser.Xp = Convert.ToInt32(Xp);
+            User.currentUser.Xp = Convert.ToInt32(Xp);
+
+
+                if (User.All[0].Id == Id)
+                {
+                    User.All[0].Xp = Convert.ToInt32(Xp);
+                }
+
+
+                for (int i = 0; i < User.All.Count; i++)
+                {
+                    if (User.All[i].Id == Id)
+                    {
+                        User.All[i].Xp = Convert.ToInt32(Xp);
+                        break;
+                    }
+                }
+
+                SetXp = "";
             await UserService.UpdateXp(Id, Convert.ToInt32(Xp));
             }
         }
@@ -71,22 +101,26 @@ namespace OpenForum.ViewModel
             IsBusy = true;
             await UserService.UpdateAdminNote(Id, Note);
             User.currentUser.Note = Note;
-            int index;
 
             for (int i = 0; i < User.All.Count; i++)
             {
                 if (User.All[i].Id == Id)
                 {
-                    index = i;
-                    User.All[index].Note = Note;
+                    User.All[i].Note = Note;
                     break;
                 }
             }
             IsBusy = false;
         }
 
-       
-  
+
+        [RelayCommand]
+        void NavigateToUsersPage()
+        {
+            Shell.Current.GoToAsync($"../../{nameof(UsersPage)}", false);
+        }
+
+
     }
 
 
